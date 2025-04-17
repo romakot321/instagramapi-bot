5164d9001103f03580937ad5a39fa1f3972088f8
romakot321 <play62342@gmail.com>
2025-04-17 19:32:52 +0300

    Add proxychains4

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd lib/python3.13/site-packages/db && alembic -c alembic.prod.ini upgrade head && cd /app
-gunicorn api.main:fastapi_app -w 1 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:80 --forwarded-allow-ips="*"
+proxychains4 gunicorn api.main:fastapi_app -w 1 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:80 --forwarded-allow-ips="*"
